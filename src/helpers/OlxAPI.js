baf60c37f8ef370e863dbf7724cfa0b4a507dd7e
Thiago Silva Pereira <thiagoufma@gmail.com>
2021-03-07 17:07:38 -0300

    Login (2 - estrutura)

---
+++ src/helpers/OlxAPI.js
@@ -0,0 +1,9 @@
+const OlxAPI = {
+  login: async (email, password) => {
+    //Fazer consulta ao WB
+
+    return {};
+  },
+};
+
+export default () => OlxAPI;
--- src/pages/Signin/index.js
+++ src/pages/Signin/index.js
@@ -1,36 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 import { PageArea } from "./styled";
+import useAPI from "../../helpers/OlxAPI";
+import { doLogin } from "../../helpers/AuthHandler";
 
 import { PageContainer, PageTitle } from "../../components/MainComponents";
 
 const Page = () => {
+  const api = useAPI();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [remenberPassword, setRemenberPassword] = useState(false);
+  const [disabled, setDisabled] = useState(false);
+  const [error, setError] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setDisabled(true);
+
+    const json = await api.login(email, password);
+
+    if (json.error) {
+      setError(json.error);
+    } else {
+      doLogin(json.token, remenberPassword);
+      window.location.href = "/";
+    }
+  };
+
   return (
     <PageContainer>
       <PageTitle>Login</PageTitle>
       <PageArea>
-        <form>
+        <form onSubmit={handleSubmit}>
           <label className="area">
             <div className="area--title">E-Mail</div>
             <div className="area--input">
-              <input type="email" />
+              <input type="email" disabled={disabled} />
             </div>
           </label>
           <label className="area">
             <div className="area--title">Senha</div>
             <div className="area--input">
-              <input type="password" />
+              <input type="password" disabled={disabled} />
             </div>
           </label>
           <label className="area">
             <div className="area--title">Lembrar Senha</div>
             <div className="area--input">
-              <input type="checkbox" />
+              <input type="checkbox" disabled={disabled} />
             </div>
           </label>
           <label className="area">
             <div className="area--title"></div>
             <div className="area--input">
-              <button>Fazer Login</button>
+              <button disabled={disabled}>Fazer Login</button>
             </div>
           </label>
         </form>
