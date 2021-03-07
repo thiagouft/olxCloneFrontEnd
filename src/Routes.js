9dea1c4ba9194a1ab1a927e042d91cfb98ddb26e
Thiago Silva Pereira <thiagoufma@gmail.com>
2021-03-07 15:25:08 -0300

    Página 404

--- src/Routes.js
+++ src/Routes.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
 
-import Home from './pages/Home';
-import About from './pages/About';
+import Home from "./pages/Home";
+import About from "./pages/About";
+import NotFound from "./pages/NotFound";
 
 export default () => {
   return (
     <Switch>
       <Route exact path="/">
-        <Home/>
+        <Home />
       </Route>
       <Route exact path="/about">
-        <About/>
+        <About />
+      </Route>
+      <Route>
+        <NotFound />
       </Route>
     </Switch>
   );
---
+++ src/pages/NotFound/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Page = () => {
+  return (
+    <div>
+      <h1>Página Não Encontada</h1>
+      <Link to="/">Voltar para Home</Link>
+    </div>
+  );
+};
+
+export default Page;
