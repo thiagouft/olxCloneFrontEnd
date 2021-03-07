3665ef8745844693e3d1fbe99abaff4d6d51e5a2
Thiago Silva Pereira <thiagoufma@gmail.com>
2021-03-07 14:47:52 -0300

    Criando o Header (2 - menu)

--- src/components/partials/Header/styled.js
+++ src/components/partials/Header/styled.js
@@ -32,4 +32,47 @@ export const HeaderArea = styled.div`
     .logo-2 { color: #00FF00 }
     .logo-3 { color: #0000FF }
   }
+
+  nav {
+    padding-top: 10px;
+    padding-bottom: 0;
+
+    ul, li {
+      margin: 0;
+      padding: 0;
+      list-style: none;
+    }
+
+    ul {
+      display: flex;
+      align-items: center;
+      height: 40px;
+    }
+
+    li {
+      margin-left: 20px;
+      margin-right: 20px;
+
+      a {
+        color: #000;
+        font-size: 14px;
+        text-decoration: none;
+
+        &:hover {
+          color: #999;
+        }
+
+        &.button {
+          background-color: #ff8100;
+          border-radius: 4px;
+          color: #FFF;
+          padding: 5px 10px;
+        }
+
+        &.button:hover {
+          background-color: #e57706;
+        }
+      }
+    }
+  }
 `;
