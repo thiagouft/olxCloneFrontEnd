67ddf8f57e633f6622f93f7a4bb380d82c164c09
Thiago Silva Pereira <thiagoufma@gmail.com>
2021-03-07 16:18:41 -0300

    Login (1 - estrutura)

--- src/Routes.js
+++ src/Routes.js
@@ -4,6 +4,7 @@ import { Switch, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import NotFound from "./pages/NotFound";
+import Signin from "./pages/Signin";
 
 export default () => {
   return (
@@ -13,6 +14,9 @@ export default () => {
       </Route>
       <Route exact path="/about">
         <About />
+      </Route>
+      <Route exact path="/signin">
+        <Signin />
       </Route>
       <Route>
         <NotFound />
