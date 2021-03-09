f6220f241ab72473702bc4012853f38b9dd568f5
Thiago Silva Pereira <thiagoufma@gmail.com>
2021-03-08 23:12:26 -0300

    Home (2 - Categoria)

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import { PageArea, SearchArea } from "./styled";
 import useAPI from "../../helpers/OlxAPI";
 
@@ -7,6 +8,25 @@ import { PageContainer } from "../../components/MainComponents";
 const Page = () => {
   const api = useAPI();
 
+  const [stateList, setStateList] = useState([]);
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    const getStates = async () => {
+      const slist = await api.getStates();
+      setStateList(slist);
+    };
+    getStates();
+  }, []);
+
+  useEffect(() => {
+    const getCategories = async () => {
+      const cats = await api.getCategories();
+      setCategories(cats);
+    };
+    getCategories();
+  }, []);
+
   return (
     <>
       <SearchArea>
@@ -14,11 +34,24 @@ const Page = () => {
           <div className="searchBox">
             <form method="GET" action="/ads">
               <input type="text" name="q" placeholder="O que vc procura??" />
-              <select name="states"></select>
+              <select name="states">
+                {stateList.map((i, k) => (
+                  <option key={k} value={i.name}>
+                    {i.name}
+                  </option>
+                ))}
+              </select>
               <button>Pesquisar</button>
             </form>
           </div>
-          <div className="categoryList"></div>
+          <div className="categoryList">
+            {categories.map((i, k) => (
+              <Link key={k} className="categoryItem" to={`/ads?cat=${i.slug}`}>
+                <img src={i.img} alt="" />
+                <span>{i.name}</span>
+              </Link>
+            ))}
+          </div>
         </PageContainer>
       </SearchArea>
       <PageContainer>
