e4085cb5e58ccb91aab87c99ee91f3bbf3ada1a3
Thiago Silva Pereira <thiagoufma@gmail.com>
2021-03-08 21:44:17 -0300

    Cadastro (2 - funcionalidade)

--- src/helpers/OlxAPI.js
+++ src/helpers/OlxAPI.js
@@ -54,6 +54,16 @@ const OlxAPI = {
     return json;
   },
 
+  register: async (name, email, password, stateLoc) => {
+    const json = await apiFetchPost("/user/signup", {
+      name,
+      email,
+      password,
+      state: stateLoc,
+    });
+    return json;
+  },
+
   getStates: async () => {
     const json = await apiFetchGet("/states");
     return json.states;
--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -33,15 +33,22 @@ const Page = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     setDisabled(true);
-    /*
-    const json = await api.login(email, password);
+    setError("");
+
+    if (password !== confirmPassword) {
+      setError("Senhas não batem");
+      setDisabled(false);
+      return;
+    }
+
+    const json = await api.register(name, email, password, stateLoc);
 
     if (json.error) {
       setError(json.error);
     } else {
-      doLogin(json.token, remenberPassword);
+      doLogin(json.token);
       window.location.href = "/";
-    }*/
+    }
     setDisabled(false);
   };
 
