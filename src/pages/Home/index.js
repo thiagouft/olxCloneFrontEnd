43690280598a82c80036412f500fcc0c26d2034d
Thiago Silva Pereira <thiagoufma@gmail.com>
2021-03-09 22:46:14 -0300

    Home (3 - Anúncios Recentes)

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,12 +4,14 @@ import { PageArea, SearchArea } from "./styled";
 import useAPI from "../../helpers/OlxAPI";
 
 import { PageContainer } from "../../components/MainComponents";
+import AdItem from "../../components/partials/AdItem";
 
 const Page = () => {
   const api = useAPI();
 
   const [stateList, setStateList] = useState([]);
   const [categories, setCategories] = useState([]);
+  const [adList, setAdList] = useState([]);
 
   useEffect(() => {
     const getStates = async () => {
@@ -25,6 +27,17 @@ const Page = () => {
       setCategories(cats);
     };
     getCategories();
+  }, []);
+
+  useEffect(() => {
+    const getRecentAds = async () => {
+      const json = await api.getAds({
+        sort: "desc",
+        limit: 8,
+      });
+      setAdList(json.ads);
+    };
+    getRecentAds();
   }, []);
 
   return (
@@ -55,7 +68,27 @@ const Page = () => {
         </PageContainer>
       </SearchArea>
       <PageContainer>
-        <PageArea>...</PageArea>
+        <PageArea>
+          <h2>Anúncios Recentes</h2>
+          <div className="list">
+            {adList.map((i, k) => (
+              <AdItem key={k} data={i} />
+            ))}
+          </div>
+          <Link to="/ads" className="seeAllLink">
+            Ver todos
+          </Link>
+          <hr />
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries, but also the leap into electronic typesetting,
+          remaining essentially unchanged. It was popularised in the 1960s with
+          the release of Letraset sheets containing Lorem Ipsum passages, and
+          more recently with desktop publishing software like Aldus PageMaker
+          including versions of Lorem Ipsum.
+        </PageArea>
       </PageContainer>
     </>
   );
--- src/pages/Home/styled.js
+++ src/pages/Home/styled.js
@@ -76,4 +76,21 @@ export const SearchArea = styled.div`
   }
 `;
 
-export const PageArea = styled.div``;
+export const PageArea = styled.div`
+  h2 {
+    font-size: 20px;
+  }
+
+  .list {
+    display: flex;
+    flex-wrap: wrap;
+  }
+
+  .seeAllLink {
+    color: #000;
+    text-decoration: none;
+    font-weight: bold;
+    display: inline-block;
+    margin-top: 10px;
+  }
+`;
