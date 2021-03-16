691ba50a7c5f782916dc47e6163413b98691cb1b
Thiago Silva Pereira <thiagoufma@gmail.com>
2021-03-15 21:35:07 -0300

    Home (4 - AdItem)

--- src/components/partials/AdItem/index.js
+++ src/components/partials/AdItem/index.js
@@ -1,5 +1,25 @@
 import React from "react";
+import { Item } from "./styled";
+import { Link } from "react-router-dom";
 
-export default () => {
-  return <div>...</div>;
+export default (props) => {
+  let price = "";
+
+  if (props.data.priceNegotiable) {
+    price = "Preço Negociavél";
+  } else {
+    price = `R$ ${props.data.price}`;
+  }
+
+  return (
+    <Item className="aditem">
+      <Link to={`/ad/${props.data.id}`}>
+        <div className="itemImage">
+          <img src={props.data.image} alt="" />
+        </div>
+        <div className="itemName">{props.data.title}</div>
+        <div className="itemPrice">{price}</div>
+      </Link>
+    </Item>
+  );
 };
--- src/pages/Home/styled.js
+++ src/pages/Home/styled.js
@@ -84,6 +84,10 @@ export const PageArea = styled.div`
   .list {
     display: flex;
     flex-wrap: wrap;
+
+    .aditem {
+      width: 25%;
+    }
   }
 
   .seeAllLink {
