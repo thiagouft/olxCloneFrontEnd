3f9e9f214c671c65bd94038253fc3c55953f5985
Thiago Silva Pereira <thiagoufma@gmail.com>
2021-03-16 20:27:10 -0300

    AdPage (6 - BreadCrumb)

--- src/pages/AdPage/index.js
+++ src/pages/AdPage/index.js
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { Slide } from "react-slideshow-image";
 import "react-slideshow-image/dist/styles.css";
-import { PageArea, Fake } from "./styled";
+import { PageArea, Fake, OthersArea, BreadChumb } from "./styled";
 import useAPI from "../../helpers/OlxAPI";
 
 import { PageContainer } from "../../components/MainComponents";
+import AdItem from "../../components/partials/AdItem";
 
 const Page = () => {
   const api = useAPI();
@@ -48,6 +49,19 @@ const Page = () => {
 
   return (
     <PageContainer>
+      {adInfo.category && (
+        <BreadChumb>
+          Você está aqui:
+          <Link to="/">Home</Link>/
+          <Link to={`/ads?state=${adInfo.stateName}`}>{adInfo.stateName}</Link>/
+          <Link
+            to={`/ads?state=${adInfo.stateName}&cat=${adInfo.category.slug}`}
+          >
+            {adInfo.category.name}
+          </Link>
+          {adInfo.title}
+        </BreadChumb>
+      )}
       <PageArea>
         <div className="leftSide">
           <div className="box">
@@ -110,6 +124,18 @@ const Page = () => {
           )}
         </div>
       </PageArea>
+      <OthersArea>
+        {adInfo.others && (
+          <>
+            <h2>Outras ofertas do vendedor</h2>
+            <div className="list">
+              {adInfo.others.map((i, k) => (
+                <AdItem key={k} data={i} />
+              ))}
+            </div>
+          </>
+        )}
+      </OthersArea>
     </PageContainer>
   );
 };
--- src/pages/AdPage/styled.js
+++ src/pages/AdPage/styled.js
@@ -99,3 +99,29 @@ export const PageArea = styled.div`
     }
   }
 `;
+
+export const OthersArea = styled.div`
+  h2 {
+    font-size: 20px;
+  }
+
+  .list {
+    display: flex;
+
+    .aditem {
+      width: 25%;
+    }
+  }
+`;
+
+export const BreadChumb = styled.div`
+  font-size: 13px;
+  margin-top: 20px;
+
+  a {
+    display: inline-block;
+    margin: 0px 5px;
+    text-decoration: underline;
+    color: #000;
+  }
+`;
