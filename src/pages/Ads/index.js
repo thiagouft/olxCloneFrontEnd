e90a2ca25a5132d0d7d37fc6bb3e95566e8276fa
Thiago Silva Pereira <thiagoufma@gmail.com>
2021-03-26 21:27:22 -0300

    Ads (2 - leftSide)

--- src/pages/Ads/index.js
+++ src/pages/Ads/index.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+import { useLocation, useHistory } from "react-router-dom";
 import { PageArea } from "./styled";
 import useAPI from "../../helpers/OlxAPI";
 
@@ -8,6 +8,20 @@ import AdItem from "../../components/partials/AdItem";
 
 const Page = () => {
   const api = useAPI();
+
+  const useQueryString = () => {
+    return new URLSearchParams(useLocation().search);
+  };
+
+  const query = useQueryString();
+
+  const [q, setQ] = useState(query.get("q") != null ? query.get("q") : "");
+  const [cat, setCat] = useState(
+    query.get("cat") != null ? query.get("cat") : ""
+  );
+  const [state, setState] = useState(
+    query.get("states") != null ? query.get("states") : ""
+  );
 
   const [stateList, setStateList] = useState([]);
   const [categories, setCategories] = useState([]);
@@ -45,9 +59,14 @@ const Page = () => {
       <PageArea>
         <div className="leftSide">
           <form method="GET">
-            <input type="text" name="q" />
+            <input
+              type="text"
+              name="q"
+              placeholder="O que você procura?"
+              value={q}
+            />
             <div className="filterName">Estado:</div>
-            <select name="state">
+            <select name="state" value={state}>
               <option></option>
               {stateList.map((i, k) => (
                 <option key={k} value={i.name}>
@@ -58,7 +77,12 @@ const Page = () => {
             <div className="filterName">Categoria:</div>
             <ul>
               {categories.map((i, k) => (
-                <li key={k} className="categoryItem">
+                <li
+                  key={k}
+                  className={
+                    cat == i.slug ? "categoryItem active" : "categoryItem"
+                  }
+                >
                   <img src={i.img} alt="" />
                   <span>{i.name}</span>
                 </li>
