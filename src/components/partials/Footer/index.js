16d0270ae94bf18015291b56a2eeb84b31841ee2
Thiago Silva Pereira <thiagoufma@gmail.com>
2021-03-27 21:38:26 -0300

    Rodapé

--- src/components/partials/Footer/index.js
+++ src/components/partials/Footer/index.js
@@ -1,13 +1,14 @@
-import React from 'react';
-import { FooterArea } from './styled';
-
+import React from "react";
+import { FooterArea } from "./styled";
 
 const Footer = () => {
   return (
     <FooterArea>
-      ...
+      Todos os direitos reservados a Thiago Silva Pereira.
+      <br />
+      OLX Clone Curso B7Web
     </FooterArea>
   );
-}
+};
 
 export default Footer;
